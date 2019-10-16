0ec2c3e4be7d3d743fa9cf9c61566cc7a7cd10e5
GabrielCede <56653028+GabrielCede@users.noreply.github.com>
2019-10-16 17:47:52 -0500

    Add files via upload

---
+++ ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = 0, total = 0;
+
+            Console.WriteLine("Ingrese las horas trabajadas: ");
+            horas = int.Parse(Console.ReadLine());
+            if (horas <= 35)
+            {
+                total = horas * 15;
+            }
+            else
+            {
+                total = ((35 * 15) + (horas - 35) * 22);
+            }
+            Console.WriteLine("La cantidad que se le debe pagar es:" + total + "dólares");
+            Console.ReadKey();
+        }
+    }
+}
