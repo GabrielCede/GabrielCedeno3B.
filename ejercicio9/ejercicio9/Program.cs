8194b99cebe55806cea9bd19ca1e07c8643a75cc
GabrielCede <56653028+GabrielCede@users.noreply.github.com>
2019-10-18 17:56:21 -0500

    Add files via upload

---
+++ ejercicio9/ejercicio9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la tabla deseada:");
+            int m = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 15; i++)
+            {
+                m = n * i;
+                Console.WriteLine($"{n} x {i} = {m}");
+            }
+        }
+    }
+}
