2d3acb3f6478e7cac8fda09498caf92a8635869b
GabrielCede <56653028+GabrielCede@users.noreply.github.com>
2019-10-18 16:51:42 -0500

    Add files via upload

---
+++ ejercicio7/ejercicio7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que escriba dos columnas de números, en la primera se colocan los números del 1 al 100, en la segunda los números del 100 al 1.
+            int columna1 = 0;
+            int columna2 = 101;
+            for (int i = 1; i <= 100; i++)
+            {
+                columna1++;
+                columna2--;
+                Console.WriteLine(columna1 + " " + columna2);
+
+            }
+        }
+    }
+}
